0b5603697cbe48a0300f47691d184a1520f7696e
Karen Huaulme <karen.huaulme@gmail.com>
2023-04-30 13:08:20 -0500

    reworking with useLazyQuery

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { APP_ID } from "../index";
 // import whichever Apollo hooks you're using
-import { useQuery, useMutation } from "@apollo/client";
-import gql from "graphql-tag";
+import { useLazyQuery } from "@apollo/client";
 import { FIND_PLAYER } from "../graphql-operations";
 
 import Header from "./Header";
@@ -14,78 +13,94 @@ const Home = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [submitted, setSubmitted] = useState(false);
   const [showNeedEndpointMessage, setShowNeedEndpointMessage] = useState(false);
-  const { loading, data } = useQuery(FIND_PLAYER, {
-    variables: { query: { short_name: searchTerm } },
-  });
+  // const { loading, data } = useQuery(FIND_PLAYER, {
+  //   variables: { query: { short_name: searchTerm } },
+  // });
+
+  // const PLAYER = data ? data.player : null;
 
   // INSERT YOUR CREATED ENDPOINT
-  const GET_PLAYERS_ENDPOINT =
-    "https://europe-west1.gcp.data.mongodb-api.com/app/karenappservice-jwjzd/endpoint/players";
+  // const GET_PLAYERS_ENDPOINT =
+  //   "https://europe-west1.gcp.data.mongodb-api.com/app/karenappservice-jwjzd/endpoint/players";
 
   // https://europe-west1.gcp.realm.mongodb.com/api/client/v2.0/app/karenappservice-jwjzd/graphql
   // https://europe-west1.gcp.data.mongodb-api.com/app/karenappservice-jwjzd/endpoint/players
 
-  const fetchPlayers = async (searchTerm) => {
-    console.log("HITTING FETCH PLAYERS API");
-    console.log("SEARCHTERM: ", searchTerm);
+  //  const FetchPlayers = async (searchTerm) => {
 
-    try {
-      // BASIC SEARCH - append searchTerm as URL parameter to GET endpoint
-      const endpoint = GET_PLAYERS_ENDPOINT + "?searchTerm=" + searchTerm;
-      const results = await (await fetch(endpoint)).json();
-      setPlayers(results.players);
-      console.log("PLAYERS: ", results.players);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // BASIC SEARCH - append searchTerm as URL parameter to GET endpoint
+  // const endpoint = GET_PLAYERS_ENDPOINT + "?searchTerm=" + searchTerm;
+  // const results = await (await fetch(endpoint)).json();
+  // setPlayers(results.players);
+  // const PLAYER = data ? data.player : null;
+  // console.log("PLAYES: ", PLAYER);
+
+  // };
+
+  const [getPlayer, { loading, error, data }] = useLazyQuery(FIND_PLAYER, {
+    variables: { query: { short_name: searchTerm } },
+  });
 
   useEffect(() => {
     if (!submitted) return;
-    if (GET_PLAYERS_ENDPOINT === "") {
-      setShowNeedEndpointMessage(true);
-      return;
-    }
-    setShowNeedEndpointMessage(false);
-    fetchPlayers(searchTerm);
+
+    console.log("SUBMITTED");
+
+    // if (GET_PLAYERS_ENDPOINT === "") {
+    //   setShowNeedEndpointMessage(true);
+    //   return;
+    // }
+    // setShowNeedEndpointMessage(false);
+    getPlayer({ variables: { query: { short_name: searchTerm } } });
+    //  FetchPlayers(searchTerm);
     setSubmitted(false);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [submitted]);
 
-  return (
-    <>
-      {" "}
-      <Header
-        searchTerm={searchTerm}
-        setSearchTerm={setSearchTerm}
-        setPlayers={setPlayers}
-        setSubmitted={setSubmitted}
-      />
-      <div className="container">
-        <img
-          src="https://images.unsplash.com/photo-1508098682722-e99c43a406b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
-          alt="stadium"
-          style={{ position: "absolute", height: "full", width: "100%" }}
-          // className="w-full h-full object-cover absolute"
+  if (loading) {
+    return <h2>Loading...</h2>;
+  } else {
+    return (
+      <>
+        {" "}
+        <Header
+          searchTerm={searchTerm}
+          setSearchTerm={setSearchTerm}
+          // setPlayers={setPlayers}
+          setSubmitted={setSubmitted}
         />
-        {showNeedEndpointMessage ? (
-          <div className="needEndpoint">Build Endpoint Please 🥺</div>
-        ) : (
-          <Grid header={searchTerm ? null : "Player Search Results"}>
-            {players.map((player) => (
-              <Thumb
-                key={player._id}
-                player={player}
-                clickable
-                playerID={player._id}
-                image={player.player_face_url ? player.player_face_url : ""}
-              ></Thumb>
-            ))}
-          </Grid>
-        )}
-      </div>{" "}
-    </>
-  );
+        <div>
+          {data && data.player ? (
+            <h1>{data.player.short_name}</h1>
+          ) : (
+            "No Players Found."
+          )}
+        </div>
+        <div className="container">
+          <img
+            src="https://images.unsplash.com/photo-1508098682722-e99c43a406b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+            alt="stadium"
+            style={{ position: "absolute", height: "full", width: "100%" }}
+          />
+          {showNeedEndpointMessage ? (
+            <div className="needEndpoint">Build Endpoint Please 🥺</div>
+          ) : (
+            <Grid header={searchTerm ? null : "Player Search Results"}>
+              {players.map((player) => (
+                <Thumb
+                  key={player._id}
+                  player={player}
+                  clickable
+                  playerID={player._id}
+                  image={player.player_face_url ? player.player_face_url : ""}
+                ></Thumb>
+              ))}
+            </Grid>
+          )}
+        </div>{" "}
+      </>
+    );
+  }
 };
 
 export default Home;
--- src/graphql-operations.js
+++ src/graphql-operations.js
@@ -1,5 +1,23 @@
 import gql from "graphql-tag";
 
+// export const FIND_PLAYER = gql`
+//   query FindPlayer($query: PlayerQueryInput!) {
+//     player(query: $query) {
+//       _id
+//       short_name
+//       long_name
+//       overall
+//       club_logo_url
+//       club_name
+//       club_jersey_number
+//       player_face_url
+//       nation_flag_url
+//       nation_jersey_number
+//       player_positions
+//     }
+//   }
+// `;
+
 export const FIND_PLAYER = gql`
   query FindPlayer($query: PlayerQueryInput!) {
     player(query: $query) {
