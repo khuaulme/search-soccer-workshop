ae6e5540d3de07264cce25db8ef8c4fd45d5355b
Karen Huaulme <karen.huaulme@gmail.com>
2023-05-24 09:51:47 -0500

    setup dummy staging for demo

--- src/graphql-operations2.js
+++ src/graphql-operations2.js
@@ -1,58 +1,21 @@
-// import gql from "graphql-tag";
+import gql from "graphql-tag";
 
-// // export const FIND_PLAYER = gql`
-// //   query FindPlayer($query: PlayerQueryInput!) {
-// //     player(query: $query) {
-// //       _id
-// //       short_name
-// //       long_name
-// //       overall
-// //       club_logo_url
-// //       club_name
-// //       club_jersey_number
-// //       player_face_url
-// //       nation_flag_url
-// //       nation_jersey_number
-// //       player_positions
-// //     }
-// //   }
-// // `;
-// // {variables: { query: { short_name: searchTerm } }} PLAYERQUERYINPUT IS AN OBJECT -- this is in my query variables
+/*************************   BASIC  ************************************* */
+export const FIND_PLAYERS_BASIC = gql``;
 
-// export const FIND_PLAYERS = gql`
-//   query FindPlayer($Input: String) {
+//   query ($Input: String) {
+//     players(
+//       query: { nationality_name: $Input }
+//       limit: 3
+//       sortBy: OVERALL_DESC
+//     ) {
+
+/*************************   SEARCH   ************************************* */
+export const FIND_PLAYERS_SEARCH = gql``;
+//  query FindPlayer($Input: String) {
 //     search(input: $Input) {
-//       _id
-//       short_name
-//       long_name
-//       overall
-//       club_logo_url
-//       club_name
-//       club_jersey_number
-//       player_face_url
-//       nation_flag_url
-//       nation_jersey_number
-//       player_positions
-//       score
-//     }
-//   }
-// `;
 
-// export const FIND_PLAYERS_ADVANCED = gql`
-//   query FindPlayerAdvanced($Input: AdvancedSearchInput) {
-//     searchAdvanced(input: $Input) {
-//       _id
-//       short_name
-//       long_name
-//       overall
-//       club_logo_url
-//       club_name
-//       club_jersey_number
-//       player_face_url
-//       nation_flag_url
-//       nation_jersey_number
-//       player_positions
-//       score
-//     }
-//   }
-// `;
+/*************************   RELATED   ************************************* */
+export const FIND_RELATED_DATA = gql``;
+//query FindPlayer($Input: String) {
+//   search(input: $Input) {
